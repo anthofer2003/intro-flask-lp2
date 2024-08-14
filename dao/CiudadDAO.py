e10b3690db579422d486403cd31dd5831473bf96
Anthony Fernandez <fernandezanthony260@gmail.com>
2024-08-14 19:59:42 -0400

    practicamos conexiones a BD, metodo GET, etc

---
+++ dao/CiudadDAO.py
@@ -0,0 +1,22 @@
+# Data access object - DAO
+
+from conexion.Conexion import Conexion
+
+class CiudadDao:
+
+    def getCiudades(self):
+
+        CiudadSQL = """
+        SELECT id. descripcion
+        FROM ciudades 
+        """
+        # objeto conexion
+        conexion = Conexion()
+        con = conexion.getConexion()
+        try:
+            pass
+        except:
+            pass
+        finally:
+            pass
+        
